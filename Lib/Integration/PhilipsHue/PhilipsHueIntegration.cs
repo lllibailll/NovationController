9049764b1616a0accf7b2370e360992742fec7d3
lllibailll <lllibailll20@gmail.com>
2020-06-15 06:09:03 +0200

    Philips Hue integration now has color and brightness changes

--- Lib/Integration/PhilipsHue/PhilipsHueIntegration.cs
+++ Lib/Integration/PhilipsHue/PhilipsHueIntegration.cs
@@ -8,15 +8,20 @@ using Lib.Integration.PhilipsHue.Model;
 using Lib.Integration.PhilipsHue.Model.Interact;
 using Lib.Manager;
 using Newtonsoft.Json;
+using RestSharp;
 
 namespace Lib.Integration.PhilipsHue
 {
     public class PhilipsHueIntegration : BaseIntegration
     {
-        private WebClient _webClient;
+        private RestClient _restClient;
         private PhilipsHueConfig _config;
-        
+
         private Dictionary<int, SmartItem> _items = new Dictionary<int, SmartItem>();
+        
+        // Endpoints
+        private string BaseEndpoint => $"{_config.PhilipsUrl}/{_config.PhilipsToken}";
+        private string LightsEndpoint => $"{BaseEndpoint}/lights";
 
         public PhilipsHueIntegration(Lib.NovationController novationController, string name, string actionPrefix) : base(novationController, name, actionPrefix)
         {
@@ -24,7 +29,7 @@ namespace Lib.Integration.PhilipsHue
 
         public override void OnLoad()
         {
-            _webClient = new WebClient();
+            _restClient = new RestClient(BaseEndpoint);
             
             LoadItems();
         }
@@ -43,17 +48,63 @@ namespace Lib.Integration.PhilipsHue
             {
                 return;
             }
-            
-            var res = _webClient.UploadString($"{LightsEndpoint}/{id}/state", WebRequestMethods.Http.Put, JsonConvert.SerializeObject(new StateToggle
-            {
-                On = !item.State.On
-            }));
+
+            SetStatus(clickableButton, id, !item.State.On);
 
             item.State.On = !item.State.On;
             
             CheckButtonColor(clickableButton, GetById(id));
+        }
+
+        private void SetStatus(ClickableButton clickableButton, int id, bool status)
+        {
+            var item = GetById(id);
+
+            if (item == null)
+            {
+                return;
+            }
+            
+            if (item.State.On == status) return;
+            
+            var req = new RestRequest($"/lights/{id}/state");
+            req.AddJsonBody(JsonConvert.SerializeObject(new StateToggle
+            {
+                On = status
+            }));
+            
+            var res = _restClient.Put(req).Content;
             
             Log.Debug($"Toggle {id}: {res}");
+        }
+
+        private void SetColor(ClickableButton clickableButton, int id, int r, int g, int b, int brightness)
+        {
+            var item = GetById(id);
+
+            if (item == null)
+            {
+                return;
+            }
+
+            var philipsColor = RgbToXy(r, g, b);
+            
+            var colorToggle = new ColorToggle
+            {
+                Brightness = brightness,
+                XY = new List<double>
+                {
+                    philipsColor.X,
+                    philipsColor.Y
+                }
+            };
+
+            var req = new RestRequest($"/lights/{id}/state");
+            req.AddJsonBody(JsonConvert.SerializeObject(colorToggle));
+            
+            var res = _restClient.Put(req).Content;
+            
+            Log.Debug($"Color res: {res}");
         }
 
         private void CheckButtonColor(ClickableButton clickableButton, SmartItem smartItem)
@@ -77,8 +128,8 @@ namespace Lib.Integration.PhilipsHue
         {
             Log.Info($"Loading items...");
             
-            var res = _webClient.DownloadString(LightsEndpoint);
-            _items = JsonConvert.DeserializeObject<Dictionary<int, SmartItem>>(res);
+            var res = _restClient.Get(new RestRequest("/lights"));
+            _items = JsonConvert.DeserializeObject<Dictionary<int, SmartItem>>(res.Content);
             
             _items.Values.ToList().ForEach(x =>
             {
@@ -86,9 +137,49 @@ namespace Lib.Integration.PhilipsHue
             });
         }
 
-        private string BaseEndpoint => $"{_config.PhilipsUrl}/{_config.PhilipsToken}";
-        private string LightsEndpoint => $"{BaseEndpoint}/lights";
-        
+        private PhilipsLight RgbToXy(int r, int g, int b)
+        {
+            var red = NormalizeRGB(r);
+            var green = NormalizeRGB(g);
+            var blue = NormalizeRGB(b);
+            
+            var valX = red * 0.664511 + green * 0.154324  + blue * 0.162028;
+            var valY = red * 0.283881 + green * 0.668433 + blue * 0.047685;
+            var valZ = red * 0.000000 + green * 0.072310 + blue * 0.986039;
+
+            var sum = valX + valY + valZ;
+
+            var philipsLight = new PhilipsLight
+            {
+                X = 0,
+                Y = 0,
+            };
+
+            if (Math.Abs(sum) > 0)
+            {
+                philipsLight.X = valX / sum;
+                philipsLight.Y = valY / sum;
+            }
+
+            return philipsLight;
+        }
+
+        private double NormalizeRGB(double input)
+        {
+            var normalized = input / 255;
+
+            if (normalized > 0.04045)
+            {
+                normalized = Math.Pow((normalized + 0.055) / (1.0 + 0.055), 2.4);
+            }
+            else
+            {
+                normalized /= 12.92;
+            }
+
+            return normalized;
+        }
+
         protected override void SetupClickAction(ClickableButton clickableButton, string[] data)
         {
             clickableButton.ClickCallbacks.Add(() =>
@@ -98,6 +189,24 @@ namespace Lib.Integration.PhilipsHue
                     case "Toggle":
                     {
                         Toggle(clickableButton, int.Parse(data[2]));
+                        break;
+                    }
+                    
+                    case "On":
+                    {
+                        SetStatus(clickableButton, int.Parse(data[2]), true);
+                        break;
+                    }
+                    
+                    case "Off":
+                    {
+                        SetStatus(clickableButton, int.Parse(data[2]), false);
+                        break;
+                    }
+
+                    case "Color":
+                    {
+                        SetColor(clickableButton, int.Parse(data[2]), int.Parse(data[3]), int.Parse(data[4]), int.Parse(data[5]), int.Parse(data[6]));
                         break;
                     }
                 }

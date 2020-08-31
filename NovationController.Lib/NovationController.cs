d003e1cbc6f160860bf27031ff20540789dc4bd0
lllibailll <lllibailll20@gmail.com>
2020-08-31 15:39:25 +0200

    Refactored namespace

--- NovationController.Lib/NovationController.cs
+++ NovationController.Lib/NovationController.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.IO;
-using Lib.Manager;
 using log4net;
 using log4net.Config;
+using NovationController.Lib.Manager;
 
-namespace Lib
+namespace NovationController.Lib
 {
     public class NovationController
     {

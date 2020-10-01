7c26b93f35e58ce3a9293c6c7a6b1c5d6baebe0b
davidarroyo1234 <davidarroyo1234@gmail.com>
2020-10-01 19:15:28 +0200

    Startup logo added.

--- NovationController.ConsoleApp/Program.cs
+++ NovationController.ConsoleApp/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace NovationController.ConsoleApp
+﻿using System;
+using System.Threading;
+
+namespace NovationController.ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(@"
+ _   _                 _   _               _____             _             _ _           
+| \ | |               | | (_)             /  __ \           | |           | | |          
+|  \| | _____   ____ _| |_ _  ___  _ __   | /  \/ ___  _ __ | |_ _ __ ___ | | | ___ _ __ 
+| . ` |/ _ \ \ / / _` | __| |/ _ \| '_ \  | |    / _ \| '_ \| __| '__/ _ \| | |/ _ \ '__|
+| |\  | (_) \ V / (_| | |_| | (_) | | | | | \__/\ (_) | | | | |_| | | (_) | | |  __/ |   
+\_| \_/\___/ \_/ \__,_|\__|_|\___/|_| |_|  \____/\___/|_| |_|\__|_|  \___/|_|_|\___|_|   
+                                                                                         
+                                                                                         
+");
             new NovationConsoleController();
         }
     }
